8a3964b16d35a5b09d48947689b75490f54adbc6
Gillen Brown <gillenbrown@gmail.com>
2018-10-03 16:36:54 -0400

    removed unneeded files, added some functions
    I don't need the compiled c files, since I have the makefile that can make them.
    
    I also added some of the recently written functions to the python wrapper

--- agb_build.py
+++ agb_build.py
@@ -2,9 +2,13 @@ from cffi import FFI
 ffibuilder = FFI()
 
 # any functions that you want to import in Python must be here. 
-ffibuilder.cdef("int find_z_index(double);" 
-                "double get_ejecta_rate(double, double);"
-                "int *find_z_bound_idxs(double z);")
+ffibuilder.cdef("void init_agb(void);"
+                "double *get_ejecta_rate(double, double);"
+                "int *find_z_bound_idxs(double);"
+                "int *find_age_bound_idxs(double);"
+                "double read_in_check(void);"
+                "double get_ages(int);"
+                "int guess_age_idx(double);")
 # in the .c file, order does matter for this compilation. In the full context of
 # ART it doesn't matter, since the .h file will be imported by the SF recipe.
 # But here, I'm only compiling this snippet, and so the order does matter. I 
